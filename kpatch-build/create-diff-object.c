bfdaa6d2b90f0357369aa23ca90639c6d32c034b
Artem Savkov <asavkov@redhat.com>
2020-10-21 13:45:52 +0200

    support non-zero sym addresses

--- kpatch-build/create-diff-object.c
+++ kpatch-build/create-diff-object.c
@@ -225,7 +225,7 @@ static void kpatch_bundle_symbols(struct kpatch_elf *kelf)
 
 	list_for_each_entry(sym, &kelf->symbols, list) {
 		if (is_bundleable(sym)) {
-			if (sym->sym.st_value != 0 &&
+			if (sym->sym.st_value != 0 && sym->sym.st_value != sym->sec->sh.sh_addr &&
 			    !is_gcc6_localentry_bundled_sym(sym)) {
 				ERROR("symbol %s at offset %lu within section %s, expected 0",
 				      sym->name, sym->sym.st_value,
